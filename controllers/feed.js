96f02391216924e8bf927050658c04fdbeb65148
sweetyagarwal3113-hub <sweetyagarwl3113@gmail.com>
2026-05-24 16:42:18 +0530

    changes

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,6 +1,11 @@
 exports.getPosts = (req, res, next) => {
     res.status(200).json({
-        posts: [{ title: 'First Post', 'content': 'This is the first post!' }]
+        posts: [{ _id: '1', title: 'First Post', 'content': 'This is the first post!', imageUrl: 'images/calender.png',
+            creator: {
+                name: "max"
+            },
+            createdAt: new Date()
+         }]
     });
 };
 
@@ -11,6 +16,9 @@ exports.createPost = (req, res, next) => {
     // Create post in db
     res.status(201).json({
         message: 'Post created successfully',
-        post: { id: new Date().toISOString(), title: title, content: content }
+        post: { id: new Date().toISOString(), title: title, content: content, 
+            creator: { name: 'Max' }, 
+            createdAt: new Date()            
+        }
     });
 };
