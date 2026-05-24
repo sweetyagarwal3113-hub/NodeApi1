3838d07ea4e51ab5b800f7570666825a6f7da97c
sweetyagarwal3113-hub <sweetyagarwl3113@gmail.com>
2026-05-24 16:51:40 +0530

    changes

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,3 +1,5 @@
+const { validationResult } = require('express-validator');
+
 exports.getPosts = (req, res, next) => {
     res.status(200).json({
         posts: [{ _id: '1', title: 'First Post', 'content': 'This is the first post!', imageUrl: 'images/calender.png',
@@ -10,6 +12,13 @@ exports.getPosts = (req, res, next) => {
 };
 
 exports.createPost = (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(422).json({
+            message: 'Validation Failed, entered data is incorrect.',
+            errors: errors.array()
+        });
+    }
     const title = req.body.title;
     const content = req.body.content;
     console.log(title, content);
