00f4d6a2ae694a830e2d60371d85b42fad11c572
sweetyagarwal3113-hub <sweetyagarwl3113@gmail.com>
2026-05-24 20:37:14 +0530

    changes

--- routes/feed.js
+++ routes/feed.js
@@ -10,5 +10,8 @@ router.post('/post', [
     body('content').trim().isLength({ min: 5 })
 ], feedController.createPost);
 router.get('/post/:postId', feedController.getPost);
-
+router.put('/post/:postId', [
+    body('title').trim().isLength({ min: 5 }),
+    body('content').trim().isLength({ min: 5 })
+], feedController.updatePost);
 module.exports = router;
